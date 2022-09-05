6881507ec8769b27a305e8c9db4835f86d8e314b
suganthigopi <suganthisongappan@gmail.com>
2022-09-05 11:56:02 +0530

    new task

---
+++ src/org/emp/Empolyee.java
@@ -0,0 +1,36 @@
+package org.emp;
+
+public class Empolyee {
+private void empNo() {
+	System.out.println("5464");
+
+}
+	private void empName() {
+System.out.println("suganthi");		
+	}
+	private void empDob() {
+		System.out.println("23.5.1995");
+	}
+	private void empPhone() {
+		System.out.println("897312195");
+	}
+	private void empEmail() {
+		System.out.println("suganthisongappan@gmail.com");
+	}
+	private void empAddress() {
+		System.out.println("grenns technology,chennai");
+	}
+public static void main(String args[]) {
+	Empolyee empolyeeInfo=new Empolyee();
+	empolyeeInfo.empNo();
+	empolyeeInfo.empName();
+	empolyeeInfo.empDob();
+	empolyeeInfo.empPhone();
+	empolyeeInfo.empEmail();
+	empolyeeInfo.empAddress();
+	
+}
+
+}
+
+
